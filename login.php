a050ed8ccdd96c350dd340c2b98a23a84b35280a
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-11-20 01:26:47 -0500

    Added login.php and register.php
    - Added php pages for login and registration.
    - Currently both only print out values posted from the respective forms.

---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Email: " . $_POST["email"] . "<br>";
+echo "Password: " . $_POST["password"] . "<br>";
+?>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Email: " . $_POST["email"] . "<br>";
+echo "Password: " . $_POST["password"] . "<br>";
+echo "Password2: " . $_POST["password_confirm"] . "<br>";
+?>
+
+</body>
+</html>
