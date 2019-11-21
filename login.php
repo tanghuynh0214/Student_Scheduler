ed9d4a62f8ed3f9d1deff4bbc5d116d084957fd2
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-11-20 23:15:26 -0500

    Updated login.php to Add Database Connection
    - Added sections of code to establish connection with database
    - Added function to validate user input into html characters

--- login.php
+++ login.php
@@ -3,8 +3,39 @@
 <body>
 
 <?php
-echo "Email: " . $_POST["email"] . "<br>";
-echo "Password: " . $_POST["password"] . "<br>";
+
+// database information
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// connect to database
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=student_scheduler", $username, $password);
+    // set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully<br>";
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+// validate input and store into variables
+$email = validate($_POST["email"]);
+$password = validate($_POST["password"]);
+
+// function to validate value of each input
+function validate($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// print out valid input
+echo "Email: " . $email . "<br>";
+echo "Password: " . $password . "<br>";
+
 ?>
 
 </body>
