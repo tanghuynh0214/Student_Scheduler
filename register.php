078c996004e32a16aa0ba76e3a1be9dc20f6e780
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-11-22 12:23:25 -0500

    Updated register.php to Complete Registration Page
    - register.php ported from login.php.
    - Registration page is complete for now.

--- register.php
+++ register.php
@@ -3,9 +3,64 @@
 <body>
 
 <?php
-echo "Email: " . $_POST["email"] . "<br>";
-echo "Password: " . $_POST["password"] . "<br>";
-echo "Password2: " . $_POST["password_confirm"] . "<br>";
+
+// database information
+$host = "localhost:3307";
+$db = "student_scheduler";
+$db_username = "root";
+$db_password = "";
+
+// validate input and store into variables
+$in_email = validate($_POST["email"]);
+$in_password = validate($_POST["password"]);
+$in_password2 = validate($_POST["password2"]);
+
+// function to validate value of each input
+function validate($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// if entered passwords don't match, return to auth page
+if($in_password != $in_password2) {
+    echo "Passwords don't match, please try again.";
+    header("Refresh: 3; url=http://cmsc447/auth.html");
+}
+
+// connect to database
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
+    // set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
+
+// run SQL statement to check whether email has been used
+$stmt = $conn->prepare('SELECT * FROM auth WHERE email=:email');
+$stmt->execute(['email' => $in_email]);
+// fetch email
+$fetched_email = $stmt->fetch()['email'];
+
+// if nothing returned, proceed with registration
+if(empty($fetched_email)) {
+    // run SQL statement to insert email and password into auth table
+    $stmt = $conn->prepare('INSERT INTO auth VALUES (:email, :password)');
+    $stmt->execute(['email' => $in_email, 'password' => $in_password]);
+
+    // jump back to auth page for login
+    echo "Registration successful, please login.";
+    header("Refresh: 3; url=http://cmsc447/auth.html");
+}
+// if email returned, display message
+else {
+    echo "Email already in system, please login.";
+    header("Refresh: 3; url=http://cmsc447/auth.html");
+}
+
 ?>
 
 </body>
