27133731fb7b2c74325a984416c142a687d6e77e
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-11-21 22:32:50 -0500

    Updated login.php to Modify Behavior
    - Fixed an error where an array was used instead of a string (line 43).
    - Browser is redirected to other pages after successful/failed authentication.

--- login.php
+++ login.php
@@ -40,20 +40,27 @@ catch(PDOException $e) {
 // run SQL statement to fetch credentials with matching email
 $stmt = $conn->prepare('SELECT * FROM auth WHERE email=:email');
 $stmt->execute(['email' => $in_email]);
-$fetched_password = $stmt->fetch();
 
-// check if fetched password is empty, if so, the email is not in database
+// fetch row, extract and store password value
+$fetched_password = $stmt->fetch()['password'];
+
+// check if fetched password is not empty, perform comparison with input
 if(!empty($fetched_password)) {
-    // compare the input password with password fetched from database
+    // if comparison passes, redirect to homepage
     if($in_password == $fetched_password) {
         echo "Login Successful.<br>";
+        header("Refresh: 3; url=http://cmsc447/homepage.html");
     }
+    // if comparison fails, redirect to authentication page again
     else {
         echo "Login Failed: Wrong Password.<br>";
+        header("Refresh: 3; url=http://cmsc447/auth.html");
     }
 }
+// if email not found in database, redirect to authentication page again
 else {
     echo "Incorrect Email.<br>";
+    header("Refresh: 3; url=http://cmsc447/auth.html");
 }
 
 ?>
