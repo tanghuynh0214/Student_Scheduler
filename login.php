b7895d9c6e7430287d71a67828294fccd1badacf
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-11-21 20:30:14 -0500

    Updated login.php to Add Authentication Feature
    - Added code to compare password from input with password from database.
    - Commented out various echoes.

--- login.php
+++ login.php
@@ -23,26 +23,37 @@ function validate($data) {
 }
 
 // print out valid input
-echo "Email: " . $in_email . "<br>";
-echo "Password: " . $in_password . "<br>";
+// echo "Email: " . $in_email . "<br>";
+// echo "Password: " . $in_password . "<br>";
 
 // connect to database
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
     // set PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to $db successfully<br>";
+    // echo "Connected to $db successfully<br>";
 }
 catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection Failed: " . $e->getMessage();
 }
 
 // run SQL statement to fetch credentials with matching email
 $stmt = $conn->prepare('SELECT * FROM auth WHERE email=:email');
 $stmt->execute(['email' => $in_email]);
-while ($row = $stmt->fetch())
-{
-    echo "Password for $in_email is " . $row['password'] . "<br>";
+$fetched_password = $stmt->fetch();
+
+// check if fetched password is empty, if so, the email is not in database
+if(!empty($fetched_password)) {
+    // compare the input password with password fetched from database
+    if($in_password == $fetched_password) {
+        echo "Login Successful.<br>";
+    }
+    else {
+        echo "Login Failed: Wrong Password.<br>";
+    }
+}
+else {
+    echo "Incorrect Email.<br>";
 }
 
 ?>
