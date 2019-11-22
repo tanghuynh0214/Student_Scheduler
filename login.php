2a4f9d4f65df44fda2628aa69bc0ea83131900f9
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-11-22 14:22:47 -0500

    Updated Database Credentials and Redirect Addresses

--- login.php
+++ login.php
@@ -5,10 +5,11 @@
 <?php
 
 // database information
-$host = "localhost:3307";
-$db = "student_scheduler";
-$db_username = "root";
-$db_password = "";
+$host = "localhost";
+$port = "3307";
+$db = "Scheduler";
+$db_username = "Scheduler";
+$db_password = "0q7L3ynn2YLwRJey";
 
 // validate input and store into variables
 $in_email = validate($_POST["email"]);
@@ -22,16 +23,11 @@ function validate($data) {
     return $data;
 }
 
-// print out valid input
-// echo "Email: " . $in_email . "<br>";
-// echo "Password: " . $in_password . "<br>";
-
 // connect to database
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
+    $conn = new PDO("mysql:host=$host:$port;dbname=$db", $db_username, $db_password);
     // set PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected to $db successfully<br>";
 }
 catch(PDOException $e) {
     echo "Connection Failed: " . $e->getMessage();
@@ -48,19 +44,20 @@ $fetched_password = $stmt->fetch()['password'];
 if(!empty($fetched_password)) {
     // if comparison passes, redirect to homepage
     if($in_password == $fetched_password) {
+        $_SESSION["email"] = $in_email;
         echo "Login Successful.<br>";
-        header("Refresh: 3; url=http://cmsc447/homepage.html");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/homepage");
     }
     // if comparison fails, redirect to authentication page again
     else {
         echo "Login Failed: Wrong Password.<br>";
-        header("Refresh: 3; url=http://cmsc447/auth.html");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/auth");
     }
 }
 // if email not found in database, redirect to authentication page again
 else {
     echo "Incorrect Email.<br>";
-    header("Refresh: 3; url=http://cmsc447/auth.html");
+    header("Refresh: 3; url=http://speaksnas1.synology.me/auth");
 }
 
 ?>
--- register.php
+++ register.php
@@ -5,10 +5,11 @@
 <?php
 
 // database information
-$host = "localhost:3307";
-$db = "student_scheduler";
-$db_username = "root";
-$db_password = "";
+$host = "localhost";
+$port = "3307";
+$db = "Scheduler";
+$db_username = "Scheduler";
+$db_password = "0q7L3ynn2YLwRJey";
 
 // validate input and store into variables
 $in_email = validate($_POST["email"]);
@@ -26,39 +27,40 @@ function validate($data) {
 // if entered passwords don't match, return to auth page
 if($in_password != $in_password2) {
     echo "Passwords don't match, please try again.";
-    header("Refresh: 3; url=http://cmsc447/auth.html");
+    header("Refresh: 3; url=http://speaksnas1.synology.me/auth");
 }
-
-// connect to database
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
-    // set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
-    echo "Connection Failed: " . $e->getMessage();
-}
-
-// run SQL statement to check whether email has been used
-$stmt = $conn->prepare('SELECT * FROM auth WHERE email=:email');
-$stmt->execute(['email' => $in_email]);
-// fetch email
-$fetched_email = $stmt->fetch()['email'];
-
-// if nothing returned, proceed with registration
-if(empty($fetched_email)) {
-    // run SQL statement to insert email and password into auth table
-    $stmt = $conn->prepare('INSERT INTO auth VALUES (:email, :password)');
-    $stmt->execute(['email' => $in_email, 'password' => $in_password]);
-
-    // jump back to auth page for login
-    echo "Registration successful, please login.";
-    header("Refresh: 3; url=http://cmsc447/auth.html");
-}
-// if email returned, display message
 else {
-    echo "Email already in system, please login.";
-    header("Refresh: 3; url=http://cmsc447/auth.html");
+    // connect to database
+    try {
+        $conn = new PDO("mysql:host=$host:$port;dbname=$db", $db_username, $db_password);
+        // set PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e) {
+        echo "Connection Failed: " . $e->getMessage();
+    }
+
+    // run SQL statement to check whether email has been used
+    $stmt = $conn->prepare('SELECT * FROM auth WHERE email=:email');
+    $stmt->execute(['email' => $in_email]);
+    // fetch email
+    $fetched_email = $stmt->fetch()['email'];
+
+    // if nothing returned, proceed with registration
+    if(empty($fetched_email)) {
+        // run SQL statement to insert email and password into auth table
+        $stmt = $conn->prepare('INSERT INTO auth VALUES (:email, :password)');
+        $stmt->execute(['email' => $in_email, 'password' => $in_password]);
+
+        // jump back to auth page for login
+        echo "Registration successful, please login.";
+        header("Refresh: 3; url=http://speaksnas1.synology.me/auth");
+    }
+    // if email returned, display message
+    else {
+        echo "Email already in system, please login.";
+        header("Refresh: 3; url=http://speaksnas1.synology.me/auth");
+    }
 }
 
 ?>
