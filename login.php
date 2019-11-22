df9fa7e896a2793f3f9aa3669a21bc05c7592274
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-11-21 19:56:29 -0500

    Updated login.php to Fetch Credentials from Database
    - Successfully fetched user's credentials from database that matches with the email from input.
    - Uses PDO::query() which is not secure against SQL injections.

--- login.php
+++ login.php
@@ -5,24 +5,14 @@
 <?php
 
 // database information
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-// connect to database
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=student_scheduler", $username, $password);
-    // set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully<br>";
-}
-catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+$host = "localhost";
+$db = "student_scheduler";
+$db_username = "root";
+$db_password = "";
 
 // validate input and store into variables
-$email = validate($_POST["email"]);
-$password = validate($_POST["password"]);
+$in_email = validate($_POST["email"]);
+$in_password = validate($_POST["password"]);
 
 // function to validate value of each input
 function validate($data) {
@@ -33,8 +23,27 @@ function validate($data) {
 }
 
 // print out valid input
-echo "Email: " . $email . "<br>";
-echo "Password: " . $password . "<br>";
+echo "Email: " . $in_email . "<br>";
+echo "Password: " . $in_password . "<br>";
+
+// connect to database
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
+    // set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to $db successfully<br>";
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+// run SQL statement to fetch credentials with matching email
+$stmt = $conn->query("SELECT * FROM auth WHERE email='$in_email'");
+while ($row = $stmt->fetch())
+{
+    echo $row['email'] . "<br>";
+    echo $row['password'] . "<br>";
+}
 
 ?>
 
