eec3c83fc8d515fe53b59f23afbc63700de11db9
tanghuynh0214 <tanghuynh0214@gmail.com>
2019-11-24 20:23:19 -0500

    - Fixed time period issue.

--- homepage.js
+++ homepage.js
@@ -5,9 +5,8 @@ var table = '';
 var row = 11;
 var col = 8;
 var startTime = 8;                //start time variable used for calculations
+var miliTime = 0;
 var actualTime = 0;               //actual time variable being displayed
-
-// table.setAttribute('id', 'calender');
 
 // For loop to create a header of table
 for(var i = 0; i < col; i++)
@@ -24,20 +23,21 @@ for(var i = 0; i < row; i++)
     // Time period
     if(j==0)
     {
-      actualTime = (startTime + j) % 12;
-      if(startTime <= 12 && actualTime < 10)
+      miliTime = (startTime + j) % 24;
+      actualTime = miliTime % 12;
+      if((miliTime < 12 || miliTime == 24) && actualTime < 10)
       {
         table += '<td>' + '0' + actualTime + ':00 AM' + '</td>';
       }
-      else if(startTime <= 12 && actualTime >= 10)
+      else if((miliTime < 12 || miliTime == 24) && actualTime >= 10)
       {
         table += '<td>' + actualTime + ':00 AM' + '</td>';
       }
-      else if(startTime <= 24 && actualTime < 10)
+      else if((miliTime < 24 || miliTime == 12)  && actualTime < 10)
       {
           table += '<td>' + '0' + actualTime + ':00 PM' + '</td>';
       }
-      else if(startTime < 24 && actualTime >= 10)
+      else if((miliTime < 24 || miliTime == 12) && actualTime >= 10)
       {
         table += '<td>' + actualTime + ':00 PM' + '</td>';
       }
