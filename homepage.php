c8472620aa46bbcd8e8cdafaa42454cc06435652
Vanisshen <38927698+maoyoushisi@users.noreply.github.com>
2019-12-01 22:49:33 -0500

    Updated Database Credentials and Session Demo
    - Updated corresponding variables in login.php to connect to live database.
    - Added php code in homapage.php to demonstrate the session feature.

--- homepage.php
+++ homepage.php
@@ -4,33 +4,40 @@ session_start();
 
 <!DOCTYPE html>
 <head>
-  <title>Student Scheduler</title>
-  <link rel="stylesheet" type = "text/css" href="homepage.css">
-  <div id = "header">
-    <p> Student Scheduler </p>
-  </div>
+    <title>Student Scheduler</title>
+    <link rel="stylesheet" type = "text/css" href="homepage.css">
+    <div id = "header">
+        <p> Student Scheduler </p>
+    </div>
 </head>
+
 <body>
-  <?php
-  echo "Email: " . $_SESSION['email'] . "<br>";
-  ?>
+    <?php
+    if(!isset($_SESSION['email'])) {
+        echo "You're not logged in.<br>";
+    }
+    else {
+        echo "Welcome, " . $_SESSION['email'] . "<br>";
+    }
+    ?>
 
-  <!-- Button row -->
-  <div class="buttonContent">
-    <div class = "button1">
-      <a href="mySchedule.html"> My Schedules </a>
+    <!-- Buttons -->
+    <div class="buttonContent">
+        <div class = "button1">
+        <a href="mySchedule.html"> My Schedules </a>
+        </div>
+        <div class="button2">
+        <a href="http://speaksnas1.synology.me/auth"> Login </a>
+        </div>
     </div>
-    <div class="button2">
-      <a href="http://speaksnas1.synology.me/auth"> Login </a>
-    </div>
-  </div>
-  <div id="scheduler">
-    <script src="homepage.js">
-    </script>
 
-  </div>
-  <div class="footer">
-    <small>&copy;Library Group 5 - Student Scheduler FA2019</small>
-  </div>
+    <!-- Table -->
+    <div id="scheduler">
+        <script src="homepage.js"></script>
+    </div>
+    
+    <div class="footer">
+        <small>&copy;Library Group 5 - Student Scheduler FA2019</small>
+    </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -10,9 +10,9 @@ session_start();
 // database information
 $host = "localhost";
 $port = "3307";
-$db = "student_scheduler";
-$db_username = "root";
-$db_password = "";
+$db = "Scheduler";
+$db_username = "Scheduler";
+$db_password = "0q7L3ynn2YLwRJey";
 
 // validate input and store into variables
 $in_email = validate($_POST["email"]);
@@ -49,18 +49,18 @@ if(!empty($fetched_password)) {
     if($in_password == $fetched_password) {
         $_SESSION["email"] = $in_email;
         echo "Login Successful.<br>";
-        header("Refresh: 3; url=homepage.php");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/homepage.php");
     }
     // if comparison fails, redirect to authentication page again
     else {
         echo "Login Failed: Wrong Password.<br>";
-        header("Refresh: 3; url=auth");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/auth");
     }
 }
 // if email not found in database, redirect to authentication page again
 else {
     echo "Incorrect Email.<br>";
-    header("Refresh: 3; url=auth");
+    header("Refresh: 3; url=http://speaksnas1.synology.me/auth");
 }
 
 ?>
