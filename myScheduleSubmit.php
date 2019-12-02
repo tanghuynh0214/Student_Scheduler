7dd64f3ec85f8dd3474c92870cf21486b2b68ac2
Adrian M <adrianm1@umbc.edu>
2019-12-02 16:39:07 -0500

    ` not '

--- myScheduleSubmit.php
+++ myScheduleSubmit.php
@@ -19,14 +19,14 @@ $db = "Scheduler";
 $db_username = "Scheduler";
 $db_password = "0q7L3ynn2YLwRJey";
 
-if(!isset($_SESSION['email'])) {
+if(!isset($_SESSION["email"])) {
     echo "You're not logged in.<br>";
 }
 else {
     //store vars
     $days = "";
-    if(!empty($_POST['day'])) {
-        foreach($_POST['day'] as $dow) {
+    if(!empty($_POST["day"])) {
+        foreach($_POST["day"] as $dow) {
                 $days = $days . $dow;
         }
         $shiftType = $_POST["shiftType"];
@@ -47,30 +47,30 @@ else {
         $endsched = $_POST["endsched"];
 
         //check times set
-        $validM = (strtotime($_POST["startshiftM"]) < strtotime($_POST["endshiftM"]) && strpos($days, 'M') !== false) || strpos($days, 'M') === false;
-        $validT = (strtotime($_POST["startshiftT"]) < strtotime($_POST["endshiftT"]) && strpos($days, 'T') !== false) || strpos($days, 'T') === false;
-        $validW = (strtotime($_POST["startshiftW"]) < strtotime($_POST["endshiftW"]) && strpos($days, 'W') !== false) || strpos($days, 'W') === false;
-        $validH = (strtotime($_POST["startshiftH"]) < strtotime($_POST["endshiftH"]) && strpos($days, 'H') !== false) || strpos($days, 'H') === false;
-        $validF = (strtotime($_POST["startshiftF"]) < strtotime($_POST["endshiftF"]) && strpos($days, 'F') !== false) || strpos($days, 'F') === false;
+        $validM = (strtotime($_POST["startshiftM"]) < strtotime($_POST["endshiftM"]) && strpos($days, "M") !== false) || strpos($days, "M") === false;
+        $validT = (strtotime($_POST["startshiftT"]) < strtotime($_POST["endshiftT"]) && strpos($days, "T") !== false) || strpos($days, "T") === false;
+        $validW = (strtotime($_POST["startshiftW"]) < strtotime($_POST["endshiftW"]) && strpos($days, "W") !== false) || strpos($days, "W") === false;
+        $validH = (strtotime($_POST["startshiftH"]) < strtotime($_POST["endshiftH"]) && strpos($days, "H") !== false) || strpos($days, "H") === false;
+        $validF = (strtotime($_POST["startshiftF"]) < strtotime($_POST["endshiftF"]) && strpos($days, "F") !== false) || strpos($days, "F") === false;
 
         //set times to default value if not set
-        if(strpos($days, 'M') === false){
+        if(strpos($days, "M") === false){
             $startshiftM = null;
             $endshiftM = null;
         }
-        if(strpos($days, 'T') === false){
+        if(strpos($days, "T") === false){
             $startshiftT = null;
             $endshiftT = null;
         }
-        if(strpos($days, 'W') === false){
+        if(strpos($days, "W") === false){
             $startshiftW = null;
             $endshiftW = null;
         }
-        if(strpos($days, 'H') === false){
+        if(strpos($days, "H") === false){
             $startshiftH = null;
             $endshiftH = null;
         }
-        if(strpos($days, 'F') === false){
+        if(strpos($days, "F") === false){
             $startshiftF = null;
             $endshiftF = null;
         }
@@ -93,8 +93,8 @@ else {
 
                 // run SQL statement to fetch credentials with matching email
                 $stmt = $conn->prepare("INSERT INTO " .
-                    "Schedules(Email, 'Schedule type', 'Start date', 'End date', " .
-                        "'Mon (start time)', 'Mon (end time)', 'Tues (start time)', 'Tues (end time)', 'Wed (start time)', 'Wed (end time)', 'Thurs (start time)', 'Thurs (end time)', 'Fri (start time)', 'Fri (end time)') " .
+                    "Schedules(Email, `Schedule type`, `Start date`, `End date`, " .
+                        "`Mon (start time)`, `Mon (end time)`, `Tues (start time)`, `Tues (end time)`, `Wed (start time)`, `Wed (end time)`, `Thurs (start time)`, `Thurs (end time)`, `Fri (start time)`, `Fri (end time)`) " .
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); //14 ?s
                 if($stmt->execute(array($_SESSION["email"], $shiftType, $startsched, $endsched, $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF))){
                     echo("Added schedule {$_SESSION["email"]} $shiftType $startsched to $endsched " .
@@ -111,8 +111,8 @@ else {
 
                 //get new sched vars
                 $days2 = "";
-                if(!empty($_POST['day2'])) {
-                    foreach($_POST['day2'] as $dow) {
+                if(!empty($_POST["day2"])) {
+                    foreach($_POST["day2"] as $dow) {
                             $days2 = $days2 . $dow;
                     }
                     $shiftType = $_POST["shiftType2"];
@@ -133,30 +133,30 @@ else {
                     $endsched2 = $_POST["endsched2"];
 
                     //check times set
-                    $validM = (strtotime($_POST["startshiftM2"]) < strtotime($_POST["endshiftM2"]) && strpos($days2, 'M') !== false) || strpos($days2, 'M') === false;
-                    $validT = (strtotime($_POST["startshiftT2"]) < strtotime($_POST["endshiftT2"]) && strpos($days2, 'T') !== false) || strpos($days2, 'T') === false;
-                    $validW = (strtotime($_POST["startshiftW2"]) < strtotime($_POST["endshiftW2"]) && strpos($days2, 'W') !== false) || strpos($days2, 'W') === false;
-                    $validH = (strtotime($_POST["startshiftH2"]) < strtotime($_POST["endshiftH2"]) && strpos($days2, 'H') !== false) || strpos($days2, 'H') === false;
-                    $validF = (strtotime($_POST["startshiftF2"]) < strtotime($_POST["endshiftF2"]) && strpos($days2, 'F') !== false) || strpos($days2, 'F') === false;
+                    $validM = (strtotime($_POST["startshiftM2"]) < strtotime($_POST["endshiftM2"]) && strpos($days2, "M") !== false) || strpos($days2, "M") === false;
+                    $validT = (strtotime($_POST["startshiftT2"]) < strtotime($_POST["endshiftT2"]) && strpos($days2, "T") !== false) || strpos($days2, "T") === false;
+                    $validW = (strtotime($_POST["startshiftW2"]) < strtotime($_POST["endshiftW2"]) && strpos($days2, "W") !== false) || strpos($days2, "W") === false;
+                    $validH = (strtotime($_POST["startshiftH2"]) < strtotime($_POST["endshiftH2"]) && strpos($days2, "H") !== false) || strpos($days2, "H") === false;
+                    $validF = (strtotime($_POST["startshiftF2"]) < strtotime($_POST["endshiftF2"]) && strpos($days2, "F") !== false) || strpos($days2, "F") === false;
 
                     //set times to default value if not set
-                    if(strpos($days2, 'M') === false){
+                    if(strpos($days2, "M") === false){
                         $startshiftM2 = null;
                         $endshiftM2 = null;
                     }
-                    if(strpos($days2, 'T') === false){
+                    if(strpos($days2, "T") === false){
                         $startshiftT2 = null;
                         $endshiftT2 = null;
                     }
-                    if(strpos($days2, 'W') === false){
+                    if(strpos($days2, "W") === false){
                         $startshiftW2 = null;
                         $endshiftW2 = null;
                     }
-                    if(strpos($days2, 'H') === false){
+                    if(strpos($days2, "H") === false){
                         $startshiftH2 = null;
                         $endshiftH2 = null;
                     }
-                    if(strpos($days2, 'F') === false){
+                    if(strpos($days2, "F") === false){
                         $startshiftF2 = null;
                         $endshiftF2 = null;
                     }
@@ -164,14 +164,14 @@ else {
                     // run SQL statement to fetch credentials with matching email
                     $stmt = $conn->prepare("UPDATE Schedules " .
                         "SET" . 
-                            "Email = ?, 'Schedule type' = ?, 'Start date' = ?, 'End date' = ?," .
-                            "'Mon (start time)' = ?, 'Mon (end time)' = ?, 'Tues (start time)' = ?, 'Tues (end time)' = ?, " . 
-                            "'Wed (start time)' = ?, 'Wed (end time)' = ?, 'Thurs (start time)' = ?, 'Thurs (end time)' = ?, " . 
-                            "'Fri (start time)' = ?, 'Fri (end time)' = ?" .
-                        "WHERE Email = ? and 'Schedule type' = ? and 'Start date' = ? and 'End date' = ? and " .
-                            "'Mon (start time)' = ? and 'Mon (end time)' = ? and 'Tues (start time)' = ? and 'Tues (end time)' = ? and " . 
-                            "'Wed (start time)' = ? and 'Wed (end time)' = ? and 'Thurs (start time)' = ? and 'Thurs (end time)' = ? and " . 
-                            "'Fri (start time)' = ? and 'Fri (end time)' = ?");
+                            "Email = ?, `Schedule type` = ?, `Start date` = ?, `End date` = ?," .
+                            "`Mon (start time)` = ?, `Mon (end time)` = ?, `Tues (start time)` = ?, `Tues (end time)` = ?, " . 
+                            "`Wed (start time)` = ?, `Wed (end time)` = ?, `Thurs (start time)` = ?, `Thurs (end time)` = ?, " . 
+                            "`Fri (start time)` = ?, `Fri (end time)` = ?" .
+                        "WHERE Email = ? and `Schedule type` = ? and `Start date` = ? and `End date` = ? and " .
+                            "`Mon (start time)` = ? and `Mon (end time)` = ? and `Tues (start time)` = ? and `Tues (end time)` = ? and " . 
+                            "`Wed (start time)` = ? and `Wed (end time)` = ? and `Thurs (start time)` = ? and `Thurs (end time)` = ? and " . 
+                            "`Fri (start time)` = ? and `Fri (end time)` = ?");
                     if($stmt->execute(array($_SESSION["email"], $shiftType2, $startsched2, $endsched2, $startshiftM2, $endshiftM2, $startshiftT2, $endshiftT2, $startshiftW2, $endshiftW2, $startshiftH2, $endshiftH2, $startshiftF2, $endshiftF2,
                         $_SESSION["email"], $shiftType, $startsched, $endsched, $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF))){
 
@@ -194,10 +194,10 @@ else {
                 # delete button was clicked
                 // run SQL statement to fetch credentials with matching email
                 $stmt = $conn->prepare("DELETE FROM Schedules " .
-                    "WHERE Email = ? and 'Schedule type' = ? and 'Start date' = ? and 'End date' = ? and " .
-                        "'Mon (start time)' = ? and 'Mon (end time)' = ? and 'Tues (start time)' = ? and 'Tues (end time)' = ? and " . 
-                        "'Wed (start time)' = ? and 'Wed (end time)' = ? and 'Thurs (start time)' = ? and 'Thurs (end time)' = ? and " . 
-                        "'Fri (start time)' = ? and 'Fri (end time)' = ?");
+                    "WHERE Email = ? and `Schedule type` = ? and `Start date` = ? and `End date` = ? and " .
+                        "`Mon (start time)` = ? and `Mon (end time)` = ? and `Tues (start time)` = ? and `Tues (end time)` = ? and " . 
+                        "`Wed (start time)` = ? and `Wed (end time)` = ? and `Thurs (start time)` = ? and `Thurs (end time)` = ? and " . 
+                        "`Fri (start time)` = ? and `Fri (end time)` = ?");
                 if($stmt->execute(array($_SESSION["email"], $shiftType, $startsched, $endsched, $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF))){
                     echo("Deleted schedule {$_SESSION["email"]} $shiftType, $startsched to $endsched " .
                         "Mon: $startshiftM - $endshiftM Tues: $startshiftT - $endshiftT Wed: $startshiftW - $endshiftW Thurs: $startshiftH - $endshiftH Fri: $startshiftF -$endshiftF");
