78c170f4f606141a745707adb3ef8c598592bc7f
Adrian M <adrianm1@umbc.edu>
2019-12-02 20:07:02 -0500

    made better significantly

--- mySchedule.php
+++ mySchedule.php
@@ -6,79 +6,68 @@
     <p> Student Scheduler </p>
   </div>
 </head>
-<script>
-  function displayEdit() {
-    // Get the checkbox
-    var checkBox = document.getElementById("cedit");
-    // Get the output text
-    var editoptions = document.getElementById("editoptions");
-
-    // If the checkbox is checked, display the output text
-    if (checkBox.checked == true){
-        editoptions.style.display = "block";
-    } else {
-        editoptions.style.display = "none";
-    }
-  }
-</script>
 <body>
   <!--Display current schedule-->
-  <?php
-    session_start();
-
-    $host = "localhost:3307";
-    $db = "Scheduler";
-    $db_username = "Scheduler";
-    $db_password = "0q7L3ynn2YLwRJey";
-
-    if(!isset($_SESSION['email'])) {
-      echo "You're not logged in.<br>";
-    }
-    else {
-        //Get your schedules
-        // connect to database
-        try {
-            $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
-            // set PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch(PDOException $e) {
-            echo "Connection Failed: " . $e->getMessage();
-        }
-
-        $stmt = $conn->prepare("SELECT * FROM Schedules WHERE Email = ?");
-        if($stmt->execute(array($_SESSION['email']))){
-            if ($stmt->rowCount() > 0) { 
-                while ($row = $stmt->fetch()) { 
-                    $currSched = $row['Email'] . " " . $row['Schedule type'] . " " . $row['Start date'] . " " . $row['End date'];
-                    //check for default times
-                    if (!is_null($row['Mon (start time)'])){
-                        $currSched = $currSched . " Mon:" . $row['Mon (start time)'] . "-" . $row['Mon (end time)'];
-                    }
-                    if (!is_null($row['Tues (start time)'])){
-                        $currSched = $currSched . " Tues:" . $row['Tues (start time)'] . "-" . $row['Tues (end time)'];
-                    }
-                    if (!is_null($row['Wed (start time)'])){
-                        $currSched = $currSched . " Wed:" . $row['Wed (start time)'] . "-" . $row['Wed (end time)'];
-                    }
-                    if (!is_null($row['Thurs (start time)'])){
-                        $currSched = $currSched . " Thurs:" . $row['Thurs (start time)'] . "-" .$row['Thurs (end time)'];
-                    }
-                    if (!is_null($row['Fri (start time)'])){
-                        $currSched = $currSched . " Fri:" . $row['Fri (start time)'] . "-" .$row['Fri (end time)'];
-                    }
-                    echo $currSched . "<br>";
-                } 
-            }
-            else{
-                echo "You have no current schedules.";
-            }
-        }
-    }
-  ?>
-  <input type="checkbox" name="Check to edit" id="cedit" onclick="displayEdit()"> Check to edit<br>
   <form id="mySchedForm" action="myScheduleSubmit.php" method="POST">
+    <?php
+        session_start();
+
+        $host = "localhost:3307";
+        $db = "Scheduler";
+        $db_username = "Scheduler";
+        $db_password = "0q7L3ynn2YLwRJey";
+
+        if(!isset($_SESSION['email'])) {
+        echo "You're not logged in.<br>";
+        }
+        else {
+            //Get your schedules
+            // connect to database
+            try {
+                $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
+                // set PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch(PDOException $e) {
+                echo "Connection Failed: " . $e->getMessage();
+            }
+
+            $stmt = $conn->prepare("SELECT * FROM Schedules WHERE Email = ?");
+            if($stmt->execute(array($_SESSION['email']))){
+                if ($stmt->rowCount() > 0) { 
+                    while ($row = $stmt->fetch()) { 
+                        $currSched = "ID: " . $row['ID'] . " " . $row['Email'] . " " . $row['Schedule type'] . " " . $row['Start date'] . " " . $row['End date'];
+                        //check for default times
+                        if (strtotime($row['Mon (start time)']) !== strtotime("00:00")){
+                            $currSched = $currSched . " Mon:" . $row['Mon (start time)'] . "-" . $row['Mon (end time)'];
+                        }
+                        if (strtotime($row['Tues (start time)']) !== strtotime("00:00")){
+                            $currSched = $currSched . " Tues:" . $row['Tues (start time)'] . "-" . $row['Tues (end time)'];
+                        }
+                        if (strtotime($row['Wed (start time)']) !== strtotime("00:00")){
+                            $currSched = $currSched . " Wed:" . $row['Wed (start time)'] . "-" . $row['Wed (end time)'];
+                        }
+                        if (strtotime($row['Thurs (start time)']) !== strtotime("00:00")){
+                            $currSched = $currSched . " Thurs:" . $row['Thurs (start time)'] . "-" .$row['Thurs (end time)'];
+                        }
+                        if (strtotime($row['Fri (start time)']) !== strtotime("00:00")){
+                            $currSched = $currSched . " Fri:" . $row['Fri (start time)'] . "-" .$row['Fri (end time)'];
+                        }
+                        $radioScheds = "<input type='radio' name='idradio' value='{$row['ID']}'>";
+                        echo $radioScheds . $currSched . "<br>";
+                    } 
+                }
+                else{
+                    echo "You have no current schedules.<br>";
+                }
+            }
+        }
+    ?>
+
+<input type="submit" name="delete" value="Delete" id="s"> Choose from above. <br>
+
       <!--Options: MTWThF/(reg/temp/absent)/shift(start,end)/days(start,end)-->
+
       <input type="radio" name="shiftType" value="regular"> Regular<br>
       <input type="radio" name="shiftType" value="temporary"> Temporary<br>
       <input type="radio" name="shiftType" value="absent"> Absent<br>
@@ -130,65 +119,11 @@
           value="2020-01-12"
           min="2019-01-01" max="2099-12-31"><br>
 
+    <input type="number" name="idnum" min="1" max="99999999999"> ID for schedule. Must be unique. <br>
+
       <!--Submits: create/edit/delete-->
-      <input type="submit" name="create" value="Create" id="c">
-      <input type="submit" name="delete" value="Delete" id="s">
-
-      <div id='editoptions' style="display:none">
-          New Schedule: <br>
-        <input type="radio" name="shiftType2" value="regular"> Regular<br>
-        <input type="radio" name="shiftType2" value="temporary"> Temporary<br>
-        <input type="radio" name="shiftType2" value="absent"> Absent<br>
-
-        <input type="checkbox" name="day2[]" value="M"> M<br>
-      <input type="time" name="startshiftM2" id="shift-start"
-          value="8:00"
-          min="08:00" max="18:00">
-      <input type="time" name="endshiftM2" id="shift-end"
-          value="18:00"
-          min="08:00" max="18:00"><br>
-
-      <input type="checkbox" name="day2[]" value="T"> T<br>
-      <input type="time" name="startshiftT2" id="shift-start"
-          value="8:00"
-          min="08:00" max="18:00">
-      <input type="time" name="endshiftT2" id="shift-end"
-          value="18:00"
-          min="08:00" max="18:00"><br>
-
-      <input type="checkbox" name="day2[]" value="W"> W<br>
-      <input type="time" name="startshiftW2" id="shift-start"
-          value="8:00"
-          min="08:00" max="18:00">
-      <input type="time" name="endshiftW2" id="shift-end"
-          value="18:00"
-          min="08:00" max="18:00"><br>
-
-      <input type="checkbox" name="day2[]" value="H"> TH<br>
-      <input type="time" name="startshiftH2" id="shift-start"
-          value="8:00"
-          min="08:00" max="18:00">
-      <input type="time" name="endshiftH2" id="shift-end"
-          value="18:00"
-          min="08:00" max="18:00"><br>
-
-      <input type="checkbox" name="day2[]" value="F"> F<br>
-      <input type="time" name="startshiftF2" id="shift-start"
-          value="8:00"
-          min="08:00" max="18:00">
-      <input type="time" name="endshiftF2" id="shift-end"
-          value="18:00"
-          min="08:00" max="18:00"><br>
-
-        <input type="date" name="startsched2" id="sched-start"
-            value="2020-01-12"
-            min="2019-01-01" max="2099-12-31">
-        <input type="date" name="endsched2" id="sched-end"
-            value="2020-01-12"
-            min="2019-01-01" max="2099-12-31"><br>
-
-          <input type="submit" name="edit" value="Edit">
-      </div>
+        <input type="submit" name="create" value="Create" id="c"> <br>
+        <input type="submit" name="edit" value="Edit"> Choose from current schedules, then set new values. <br>
     </form>
 </body>
 </html>
--- myScheduleSubmit.php
+++ myScheduleSubmit.php
@@ -8,9 +8,14 @@ session_start();
 
 <?php
 //TODOs: 
-//check if logged in
-//actual sql statements
-//better define how edit works
+//how to use text
+//check for 00 not null
+//add ids to schedules
+//delete/edit based on id
+//redirect after submit
+//24hr
+//css
+//id option
 
 
 // database information
@@ -21,207 +26,159 @@ $db_password = "0q7L3ynn2YLwRJey";
 
 if(!isset($_SESSION["email"])) {
     echo "You're not logged in.<br>";
+    header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
 }
-else {
+
+// connect to database
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
+    // set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+}
+
+if (isset($_POST["delete"])) {
+    # delete button was clicked
+
+    if(!isset( $_POST["idradio"])){
+        echo ("ID not set.<br>");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+    }
+    $id = $_POST["idradio"];
+
+    // run SQL statement to fetch credentials with matching email
+    $stmt = $conn->prepare("DELETE FROM Schedules WHERE Email = ? and `ID` = ?");
+    if($stmt->execute(array($_SESSION["email"], $id))){
+        echo("Deleted success");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+    }
+    //if couldnt insert redirect to mySchedule
+    else {
+        echo "Could not delete schedule in database.<br>";
+        header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+    }
+}
+elseif(isset($_POST["create"]) || isset($_POST["edit"])){
+    if(empty($_POST["day"])) {
+        echo("Select a day.");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+    }
+    
+    //check times set
+    $validM = (strtotime($_POST["startshiftM"]) < strtotime($_POST["endshiftM"]) && strpos($days, "M") !== false) || strpos($days, "M") === false;
+    $validT = (strtotime($_POST["startshiftT"]) < strtotime($_POST["endshiftT"]) && strpos($days, "T") !== false) || strpos($days, "T") === false;
+    $validW = (strtotime($_POST["startshiftW"]) < strtotime($_POST["endshiftW"]) && strpos($days, "W") !== false) || strpos($days, "W") === false;
+    $validH = (strtotime($_POST["startshiftH"]) < strtotime($_POST["endshiftH"]) && strpos($days, "H") !== false) || strpos($days, "H") === false;
+    $validF = (strtotime($_POST["startshiftF"]) < strtotime($_POST["endshiftF"]) && strpos($days, "F") !== false) || strpos($days, "F") === false;
+    
+    //check start-end of times
+    if(strtotime($startsched) < strtotime($endsched) && $validM && $validT && $validW && $validH && $validF){
+        echo("End time cannot be before start time");
+        header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+    }
+
     //store vars
     $days = "";
-    if(!empty($_POST["day"])) {
-        foreach($_POST["day"] as $dow) {
-                $days = $days . $dow;
+    foreach($_POST["day"] as $dow) {
+            $days = $days . $dow;
+    }
+    $shiftType = $_POST["shiftType"];
+
+    //get times
+    $startshiftM = $_POST["startshiftM"];
+    $endshiftM = $_POST["endshiftM"];
+    $startshiftT = $_POST["startshiftT"];
+    $endshiftT = $_POST["endshiftT"];
+    $startshiftW = $_POST["startshiftW"];
+    $endshiftW = $_POST["endshiftW"];
+    $startshiftH = $_POST["startshiftH"];
+    $endshiftH = $_POST["endshiftH"];
+    $startshiftF = $_POST["startshiftF"];
+    $endshiftF = $_POST["endshiftF"];
+
+    $startsched = $_POST["startsched"];
+    $endsched = $_POST["endsched"];
+
+    //set times to default value if not set
+    if(strpos($days, "M") === false){
+        $startshiftM = "00:00";
+        $endshiftM = "00:00";
+    }
+    if(strpos($days, "T") === false){
+        $startshiftT = "00:00";
+        $endshiftT = "00:00";
+    }
+    if(strpos($days, "W") === false){
+        $startshiftW = "00:00";
+        $endshiftW = "00:00";
+    }
+    if(strpos($days, "H") === false){
+        $startshiftH = "00:00";
+        $endshiftH = "00:00";
+    }
+    if(strpos($days, "F") === false){
+        $startshiftF = "00:00";
+        $endshiftF = "00:00";
+    }
+
+    //do one of the three different submit options
+    if (isset($_POST["create"])) {
+        // create button was clicked
+
+        $id = $_POST["idnum"];
+
+        // run SQL statement to fetch credentials with matching email
+        $stmt = $conn->prepare("INSERT INTO " .
+            "Schedules(ID, Email, `Schedule type`, `Start date`, `End date`, " .
+                "`Mon (start time)`, `Mon (end time)`, `Tues (start time)`, `Tues (end time)`, `Wed (start time)`, `Wed (end time)`, `Thurs (start time)`, `Thurs (end time)`, `Fri (start time)`, `Fri (end time)`) " .
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); //15 ?s
+        if($stmt->execute(array($id, $_SESSION["email"], $shiftType, $startsched, $endsched, $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF))){
+            echo("Added schedule {$_SESSION["email"]} $shiftType $startsched to $endsched " .
+                "Mon: $startshiftM - $endshiftM Tues: $startshiftT - $endshiftT Wed: $startshiftW - $endshiftW Thurs: $startshiftH - $endshiftH Fri: $startshiftF -$endshiftF");
         }
-        $shiftType = $_POST["shiftType"];
-        
-        //get times
-        $startshiftM = $_POST["startshiftM"];
-        $endshiftM = $_POST["endshiftM"];
-        $startshiftT = $_POST["startshiftT"];
-        $endshiftT = $_POST["endshiftT"];
-        $startshiftW = $_POST["startshiftW"];
-        $endshiftW = $_POST["endshiftW"];
-        $startshiftH = $_POST["startshiftH"];
-        $endshiftH = $_POST["endshiftH"];
-        $startshiftF = $_POST["startshiftF"];
-        $endshiftF = $_POST["endshiftF"];
-
-        $startsched = $_POST["startsched"];
-        $endsched = $_POST["endsched"];
-
-        //check times set
-        $validM = (strtotime($_POST["startshiftM"]) < strtotime($_POST["endshiftM"]) && strpos($days, "M") !== false) || strpos($days, "M") === false;
-        $validT = (strtotime($_POST["startshiftT"]) < strtotime($_POST["endshiftT"]) && strpos($days, "T") !== false) || strpos($days, "T") === false;
-        $validW = (strtotime($_POST["startshiftW"]) < strtotime($_POST["endshiftW"]) && strpos($days, "W") !== false) || strpos($days, "W") === false;
-        $validH = (strtotime($_POST["startshiftH"]) < strtotime($_POST["endshiftH"]) && strpos($days, "H") !== false) || strpos($days, "H") === false;
-        $validF = (strtotime($_POST["startshiftF"]) < strtotime($_POST["endshiftF"]) && strpos($days, "F") !== false) || strpos($days, "F") === false;
-
-        //set times to default value if not set
-        if(strpos($days, "M") === false){
-            $startshiftM = "00:00";
-            $endshiftM = "00:00";
-        }
-        if(strpos($days, "T") === false){
-            $startshiftT = "00:00";
-            $endshiftT = "00:00";
-        }
-        if(strpos($days, "W") === false){
-            $startshiftW = "00:00";
-            $endshiftW = "00:00";
-        }
-        if(strpos($days, "H") === false){
-            $startshiftH = "00:00";
-            $endshiftH = "00:00";
-        }
-        if(strpos($days, "F") === false){
-            $startshiftF = "00:00";
-            $endshiftF = "00:00";
-        }
-
-        //check start-end of times
-        if(strtotime($startsched) < strtotime($endsched) && $validM && $validT && $validW && $validH && $validF){
-            // connect to database
-            try {
-                $conn = new PDO("mysql:host=$host;dbname=$db;", $db_username, $db_password);
-                // set PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            catch(PDOException $e) {
-                echo "Connection Failed: " . $e->getMessage();
-            }
-
-            //do one of the three different submit options
-            if (isset($_POST["create"])) {
-                // create button was clicked
-
-                // run SQL statement to fetch credentials with matching email
-                $stmt = $conn->prepare("INSERT INTO " .
-                    "Schedules(Email, `Schedule type`, `Start date`, `End date`, " .
-                        "`Mon (start time)`, `Mon (end time)`, `Tues (start time)`, `Tues (end time)`, `Wed (start time)`, `Wed (end time)`, `Thurs (start time)`, `Thurs (end time)`, `Fri (start time)`, `Fri (end time)`) " .
-                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); //14 ?s
-                if($stmt->execute(array($_SESSION["email"], $shiftType, $startsched, $endsched, $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF))){
-                    echo("Added schedule {$_SESSION["email"]} $shiftType $startsched to $endsched " .
-                        "Mon: $startshiftM - $endshiftM Tues: $startshiftT - $endshiftT Wed: $startshiftW - $endshiftW Thurs: $startshiftH - $endshiftH Fri: $startshiftF -$endshiftF");
-                }
-                //if couldnt insert redirect to mySchedule
-                else {
-                    echo ("Could not add schedule to database.<br>");
-                    header("Refresh: 3; url=http://cmsc447/mySchedule.php");
-                }
-            }
-            elseif (isset($_POST["edit"])) {
-                # edit button was clicked
-
-                //get new sched vars
-                $days2 = "";
-                if(!empty($_POST["day2"])) {
-                    foreach($_POST["day2"] as $dow) {
-                            $days2 = $days2 . $dow;
-                    }
-                    $shiftType = $_POST["shiftType2"];
-
-                    //get times
-                    $startshiftM2 = $_POST["startshiftM2"];
-                    $endshiftM2 = $_POST["endshiftM2"];
-                    $startshiftT2 = $_POST["startshiftT2"];
-                    $endshiftT2 = $_POST["endshiftT2"];
-                    $startshiftW2 = $_POST["startshiftW2"];
-                    $endshiftW2 = $_POST["endshiftW2"];
-                    $startshiftH2 = $_POST["startshiftH2"];
-                    $endshiftH2 = $_POST["endshiftH2"];
-                    $startshiftF2 = $_POST["startshiftF2"];
-                    $endshiftF2 = $_POST["endshiftF2"];
-
-                    $startsched2 = $_POST["startsched2"];
-                    $endsched2 = $_POST["endsched2"];
-
-                    //check times set
-                    $validM = (strtotime($_POST["startshiftM2"]) < strtotime($_POST["endshiftM2"]) && strpos($days2, "M") !== false) || strpos($days2, "M") === false;
-                    $validT = (strtotime($_POST["startshiftT2"]) < strtotime($_POST["endshiftT2"]) && strpos($days2, "T") !== false) || strpos($days2, "T") === false;
-                    $validW = (strtotime($_POST["startshiftW2"]) < strtotime($_POST["endshiftW2"]) && strpos($days2, "W") !== false) || strpos($days2, "W") === false;
-                    $validH = (strtotime($_POST["startshiftH2"]) < strtotime($_POST["endshiftH2"]) && strpos($days2, "H") !== false) || strpos($days2, "H") === false;
-                    $validF = (strtotime($_POST["startshiftF2"]) < strtotime($_POST["endshiftF2"]) && strpos($days2, "F") !== false) || strpos($days2, "F") === false;
-
-                    //set times to default value if not set
-                    if(strpos($days2, "M") === false){
-                        $startshiftM2 = "00:00";
-                        $endshiftM2 = "00:00";
-                    }
-                    if(strpos($days2, "T") === false){
-                        $startshiftT2 = "00:00";
-                        $endshiftT2 = "00:00";
-                    }
-                    if(strpos($days2, "W") === false){
-                        $startshiftW2 = "00:00";
-                        $endshiftW2 = "00:00";
-                    }
-                    if(strpos($days2, "H") === false){
-                        $startshiftH2 = "00:00";
-                        $endshiftH2 = "00:00";
-                    }
-                    if(strpos($days2, "F") === false){
-                        $startshiftF2 = "00:00";
-                        $endshiftF2 = "00:00";
-                    }
-
-                    // run SQL statement to fetch credentials with matching email
-                    $stmt = $conn->prepare("UPDATE Schedules " .
-                        "SET" . 
-                            "Email = ?, `Schedule type` = ?, `Start date` = ?, `End date` = ?," .
-                            "`Mon (start time)` = ?, `Mon (end time)` = ?, `Tues (start time)` = ?, `Tues (end time)` = ?, " . 
-                            "`Wed (start time)` = ?, `Wed (end time)` = ?, `Thurs (start time)` = ?, `Thurs (end time)` = ?, " . 
-                            "`Fri (start time)` = ?, `Fri (end time)` = ?" .
-                        "WHERE Email = ? and `Schedule type` = ? and `Start date` = ? and `End date` = ? and " .
-                            "`Mon (start time)` = ? and `Mon (end time)` = ? and `Tues (start time)` = ? and `Tues (end time)` = ? and " . 
-                            "`Wed (start time)` = ? and `Wed (end time)` = ? and `Thurs (start time)` = ? and `Thurs (end time)` = ? and " . 
-                            "`Fri (start time)` = ? and `Fri (end time)` = ?");
-                    if($stmt->execute(array(
-                        $_SESSION["email"], $shiftType2, $startsched2, $endsched2, 
-                        $startshiftM2, $endshiftM2, $startshiftT2, $endshiftT2, $startshiftW2, $endshiftW2, $startshiftH2, $endshiftH2, $startshiftF2, $endshiftF2,
-                        $_SESSION["email"], $shiftType, 
-                        $startsched, $endsched, $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF))){
-
-                        echo("Changed schedule {$_SESSION["email"]} $shiftType $startsched to $endsched " .
-                        "Mon: $startshiftM - $endshiftM Tues: $startshiftT - $endshiftT Wed: $startshiftW - $endshiftW Thurs: $startshiftH - $endshiftH Fri: $startshiftF -$endshiftF to " .
-                        "schedule {$_SESSION["email"]} $shiftType2 $startsched2 to $endsched2 " .
-                        "Mon: $startshiftM2 - $endshiftM2 Tues: $startshiftT2 - $endshiftT2 Wed: $startshiftW2 - $endshiftW2 Thurs: $startshiftH2 - $endshiftH2 Fri: $startshiftF2 -$endshiftF2");
-                    }
-                    //if couldnt insert redirect to mySchedule
-                    else {
-                        echo ("Could not change schedule in database.<br>");
-                        header("Refresh: 3; url=http://cmsc447/mySchedule.php");
-                    }
-                }
-                else{
-                    echo("Select the day(s) to edit to.");
-                }
-            }
-            elseif (isset($_POST["delete"])) {
-                # delete button was clicked
-                // run SQL statement to fetch credentials with matching email
-                $stmt = $conn->prepare("DELETE FROM Schedules " .
-                    "WHERE Email = ? and `Schedule type` = ? and `Start date` = ? and `End date` = ? and " .
-                        "`Mon (start time)` = ? and `Mon (end time)` = ? and `Tues (start time)` = ? and `Tues (end time)` = ? and " . 
-                        "`Wed (start time)` = ? and `Wed (end time)` = ? and `Thurs (start time)` = ? and `Thurs (end time)` = ? and " . 
-                        "`Fri (start time)` = ? and `Fri (end time)` = ?");
-                if($stmt->execute(array($_SESSION["email"], $shiftType, $startsched, $endsched, $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF))){
-                    echo("Deleted schedule {$_SESSION["email"]} $shiftType, $startsched to $endsched " .
-                        "Mon: $startshiftM - $endshiftM Tues: $startshiftT - $endshiftT Wed: $startshiftW - $endshiftW Thurs: $startshiftH - $endshiftH Fri: $startshiftF -$endshiftF");
-                }
-                //if couldnt insert redirect to mySchedule
-                else {
-                    echo "Could not delete schedule in database.<br>";
-                    header("Refresh: 3; url=http://cmsc447/mySchedule.php");
-                }
-            }
-            else{
-                echo("How'd you do that?");
-            }
-        }
-        else{
-            echo("Select a day(s).");
+        //if couldnt insert redirect to mySchedule
+        else {
+            echo ("Could not add schedule to database.<br>");
+            header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
         }
     }
-    else{
-        echo("End time cannot be before start time");
+    elseif (isset($_POST["edit"])) {
+        # edit button was clicked
+
+        if(!isset( $_POST["idradio"])){
+            echo ("ID not set.<br>");
+            header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+        }
+        $id = $_POST["idradio"];
+
+        // run SQL statement to fetch credentials with matching email
+        $stmt = $conn->prepare("UPDATE Schedules " .
+            "SET" . 
+                "`Schedule type` = ?, `Start date` = ?, `End date` = ?," .
+                "`Mon (start time)` = ?, `Mon (end time)` = ?, `Tues (start time)` = ?, `Tues (end time)` = ?, " . 
+                "`Wed (start time)` = ?, `Wed (end time)` = ?, `Thurs (start time)` = ?, `Thurs (end time)` = ?, " . 
+                "`Fri (start time)` = ?, `Fri (end time)` = ?" .
+            "WHERE Email = ? and `ID` = ?");
+        if($stmt->execute(array(
+            $shiftType, $startsched, $endsched, 
+            $startshiftM, $endshiftM, $startshiftT, $endshiftT, $startshiftW, $endshiftW, $startshiftH, $endshiftH, $startshiftF, $endshiftF,
+            $_SESSION["email"], $id))){
+
+            echo("Changed success");
+            header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+        }
+        //if couldnt insert redirect to mySchedule
+        else {
+            echo ("Could not change schedule in database.<br>");
+            header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
+        }
     }
+}
+else{
+    echo("How'd you do that?");
+    header("Refresh: 3; url=http://speaksnas1.synology.me/mySchedule.php");
 }
 ?>
 
